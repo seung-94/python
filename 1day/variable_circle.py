2a3065608ee346f366859947e3786c41ef5cc12a
asd <tanaa1137@gmail.com>
2024-08-28 13:14:28 +0900

    .

---
+++ 1day/variable_circle.py
@@ -0,0 +1,11 @@
+def main():
+    pi = 3.141592
+    r = 10
+
+    print("원주율 : ", pi)
+    print("반지름 : ", r)
+    print("원의 넓이 : ", pi*r**2)
+    print("원의 둘레 : ", 2*pi*r)
+
+if __name__ == "__main__":
+    main()
