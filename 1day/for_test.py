0b56e9011ade3872c917ae6e911f22d685e1d304
asd <tanaa1137@gmail.com>
2024-08-28 15:54:15 +0900

    ..

---
+++ 1day/for_test.py
@@ -0,0 +1,28 @@
+#def main():
+#    int_i = 0
+#
+#    for i in range(10):
+#        int_i += 1
+#        print(int_i)
+#
+#if __name__ == "__main__":
+#    main()
+
+def main():
+#   for(int i=0;i<10;i++){
+#        printf("%d", i+1);
+#    }
+# for(auto i: li){...}
+    for i in range(10):
+        print(i+1)
+
+    li2 = [123, 3.14, 1.4, 7, "number"]
+
+    for ele in li2:
+        print(ele)
+
+    for i, ele in enumerate(li2):
+        print(f"index: {i}, element: {ele}")
+
+if __name__ == "__main__":
+    main()
